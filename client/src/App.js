b535a631137d403e26ac640f2c3fb57fba4cb0dd
Kofi Boakye <59366644+reyzuki@users.noreply.github.com>
2022-01-31 17:58:20 +0000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
+mport {Routes, Route} from "react-router-dom";
 import './App.css';
 import {Login, SignUp} from './pages.js';
-import { BrowserRouter as Switch, Route, Routes } from "react-router-dom";
-import Header from "./components/Header"
-import Sidebar from "./components/Sidebar"
-import Mail  from "./components/Mail";
-import EmailList from "./components/EmailList";
-
+//import restaurant from './restaurant.jpg'
 
 
 function App(){
@@ -14,23 +10,6 @@ return(
     <Routes>
        <Route path="/" element={<Login />}/>
       <Route path="/signup" element={<SignUp />}/>
-      <div className="App">
-      <Header/>
-
-      <div className="app_body">
-      <Sidebar/>
-      
-      <Switch>
-       <Route path="/mail">
-        <Mail/>
-       </Route>
-
-       <Route path="/">
-        <EmailList/>
-       </Route>
-      </Switch>
-      </div>
-    </div>
     </Routes>
   </div>
 ); 
