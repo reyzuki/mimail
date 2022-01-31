0f2bcfbf9c90fb8f7e1a83570c99d9b98fc61a07
Kofi Boakye <59366644+reyzuki@users.noreply.github.com>
2022-01-31 17:59:35 +0000

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App.js';
+import App from './App.js'
+import{BrowserRouter as Router} from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-  <App />
-</React.StrictMode>,
+    <Router>
+      <App/>
+    </Router>,
     document.getElementById('root'));
